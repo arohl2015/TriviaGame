0867c51b22dca855b4d6973a6f2154f4b6dc6301
A Rohl <april.rohl2015@gmail.com>
2020-02-28 14:59:54 -0700

    pseudocode for app.js

---
+++ assets/javascript/app.js
@@ -0,0 +1,38 @@
+// Beiginning of game - creating the variables to use in the game
+
+
+// Getting the game ready to run
+$(document).ready(function () {
+//within here create the questions and answers for the game
+
+}
+)
+
+// Start the game by clicking on the "start" button
+
+// Once you click on "start", then need to add function to start the timer
+
+// Timer is started, now the countdown needs to begin (interval function)
+
+// Allow functionality to stop the timer once it reaches 0
+
+// Within here will need to add in the vars relating to Q&A's
+// If/Else statement to clarify if user is correct or wrong 
+if (condition) {
+
+
+} else {
+    
+}
+
+// Click function as user selects choice
+
+
+// Show tally of correct/wrong answers to trivia
+
+//reset
+function reset() {
+
+}
+
+//End of Script
