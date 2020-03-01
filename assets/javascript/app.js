4d7f0d01344a3e21fda094efbb23151b7ba54e07
A Rohl <april.rohl2015@gmail.com>
2020-03-01 13:38:10 -0700

    updated html and js files w/ addnl code

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -8,15 +8,9 @@ var questionWrong = 0;
 //created placeholder var to push var questions
 var placeholder = [];
 //variable for user input 
-var guess ="";
+var guess = "";
 var right;
 var wrong;
-
-//Start game
-$(document).ready(function () {
-
-//Need to put questions below within these brackets?
-})
 
 // create the questions and answers for the game
 
@@ -46,138 +40,146 @@ var questions = [
         photo: "assets/images/ironman.gif"
     },
     {
-        question: "Britney Spears has named all her dogs these names except:"
-        choice: ["Lucy", "Bit Bit", "Hannah", "Lacy Loo"]
+        question: "Britney Spears has named all her dogs these names except:",
+        choice: ["Lucy", "Bit Bit", "Hannah", "Lacy Loo"],
         answer: 0,
         photo: "assets/images/britney.gif"
-    },
-    {
-        question:
-        choice:
-        answer:
-        photo:
-    },
-    {
-        question:
-        choice:
-        answer:
-        photo:
-    },
-    {
-        question:
-        choice:
-        answer:
-        photo:
-    },
-    {
-        question:
-        choice:
-        answer:
-        photo:
-    },
-    {
-        question:
-        choice:
-        answer:
-        photo:
-    },
-}
+    }
+    //    {
+    //         question:
+    //         choice:
+    //         answer:
+    //         photo:
+    //     },
+    //     {
+    //         question:
+    //         choice:
+    //         answer:
+    //         photo:
+    //     },
+    //     {
+    //         question:
+    //         choice:
+    //         answer:
+    //         photo:
+    //     },
+    //     {
+    //         question:
+    //         choice:
+    //         answer:
+    //         photo:
+    //     },
+    //     {
+    //         question:
+    //         choice:
+    //         answer:
+    //         photo:
+    //     },
+]
+
+$("#content").hide();
+$("#results").hide();
+$("#reset").hide();
+//Start game
+// $(document).ready(function () {
+//     
+//     //Need to put questions below within these brackets?
+// })
 
 //hide (How to hide parts of text) and reset function (will clear or reset the input form fields) to transition between question slides and start the game
-$("#reset").hide();
+
 // Start the game by clicking on the "start" button
 $("#start").on("click", function () {
-    $("#start").hide;
+    //$("#start").show();
+    $("#content").show();
     displayQuestion();
-		runTime();
-		for(var i = 0; i < questions.length; i++) {
+		//runTime();
+
             //The push() method adds new items to the end of an array, and returns the new length.
-	placeholder.push(questions[i]);
-}
+	
 })
 
 // Need to add function to start the timer
 function runTime() {
-    if (!timerRunning) {
+   // if (!timerRunning) {
         intervalId = setInterval(decrement, 1000);
         clockRunning = true;
-    }
+    //}
 }
 // Timer is started, now the countdown needs to begin (interval function)
 function decrement() {
-    timeLeft --;
+    timeLeft--;
     $("#timer").html("<h2>Time Remaining: " + timeLeft + "</h2>");
-// What happens if the timer reaches 0 before the person answers the question?
-if (timeLeft === 0) {
-    var questionNumber++;
-    stop();
-    $("#answer").html("<p>Time's up! The correct answer is: " + guess.choice[guess.answer] + "</p>");
-		hidepicture();
+    // What happens if the timer reaches 0 before the person answers the question?
+    if (timeLeft === 0) {
+        questionNumber++;
+        stop();
+        $("#answer").html("<p>Time's up! The correct answer is: " + guess.choice[guess.answer] + "</p>");
+        hidepicture();
     }
 }
 // Allow functionality to stop the timer
 function stop() {
     clearInterval(intervalId);
     timerRunning = false;
-  }
-
-// Is there where I put the questions array?
-function displayQuestion () {
-    index = Math.floor(Math.random()*questions.length);
-    guess = questions[index];
-
-if () {
-    
-} else {
-    
 }
 
+// Is there where I put the questions array?
+function displayQuestion() {
 
-
+    // index = Math.floor(Math.random()*questions.length);
+    //  guess = questions[index];
+    $("#question").text(questions[questionNumber].question);
+    $("#choice-1").text(questions[questionNumber].choice[0]);
+    $("#choice-2").text(questions[questionNumber].choice[1]);
+    $("#choice-3").text(questions[questionNumber].choice[2]);
+    $("#choice-4").text(questions[questionNumber].choice[3]);
 }
 
 // Need to add a click event regarding answers
-$(".useranswer").on("click", function () {
-guess = 
+$(".userchoice").on("click", function () {
+    guess = parseInt($(this).attr("value"));
+    console.log("guess", guess);
 
+    // If/Else statement regarding whether user selects right or wrong answer
+    if (guess === questions[questionNumber].answer) {
+        // stop();
+        questionCorrect++;
+        guess = "";
+        $("#answer").html("<p>You're Correct!</p>");
+        //hidepicture();
 
+    } else {
+        //stop();
+        questionWrong++;
+        guess = "";
+        $("#answer").html("<p>That's Wrong. The correct answer is: " + guess.choice[guess.answer] + "</p>");
+        //hidepicture();
+    }
+    if (questionNumber < questions.length - 1) {
+        questionNumber++;
+        displayQuestion();
+
+    } else {
+        displayResults();
+
+    }
 })
-
-// If/Else statement regarding whether user selects right or wrong answer
-if (guess === right) {
-    stop();
-    questionCorrect++;
-    guess="";
-    $("#answer").html("<p>You're Correct!</p>");
-    hidepicture();
-
-} else {
-    stop();
-    questionWrong++;
-    guess="";
-    $("#answer").html("<p>That's Wrong. The correct answer is: " + guess.choice[guess.answer] + "</p>");
-    hidepicture();
-}
-
 // Add in function to define hidepicture
-function hidepicture() {
-    
+function displayResults() {
+    console.log(questionCorrect, questionWrong);
+    $("#displayresults").show();
+    $("#results").append("<h3> Right Answers" + questionCorrect + "</h3>");
+    $("#results").append("<h3> Wrong Answers" + questionWrong + "</h3>");
+    $("#reset").show();
 }
 
 
 // Show tally of correct/wrong answers to trivia
 
-if () {
-    
-} else {
-    
-}
 
 //reset - is it a function or click even with the button?
-function reset() {
-
-}
-
 $("#reset").on("click", function () {
+})
 
 //End of Script
