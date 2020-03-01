7be210166baa06b9fc7185d9f065a2c67a41c8a1
A Rohl <april.rohl2015@gmail.com>
2020-03-01 10:53:41 -0700

    updated README and app.js questions

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # TriviaGame
 
-** This week's assignment was: **
+##### This week's assignment was: #####
 * You'll create a trivia game that shows only one question until the player answers it or their time runs out.
 * If the player selects the correct answer, show a screen congratulating them for choosing the right option. After a few seconds, display the next question -- do this without user input.
 * The scenario is similar for wrong answers and time-outs.
--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,14 +1,7 @@
-//Start the game
-
-$(document).ready(function () {
-startGame()
-//Need to put all the stuff below within these brackets?
-})
-
 // Creating the variables to track progress in game
 
 var questionNumber = 0;
-//will this read 30 seconds or do I need to enter in 3000?
+//will this read 30 seconds or do I need to enter in 30000?
 var timeLeft = 30;
 var questionCorrect = 0;
 var questionWrong = 0;
@@ -19,14 +12,25 @@ var guess ="";
 var right;
 var wrong;
 
+$(document).ready(function () {
+
+//Need to put questions below within these brackets?
+})
+
 //within here create the questions and answers for the game
 
 var questions = [
     {
-        question:
-        choice:
-        answer:
-        photo:
+        question: "Which Popular TV Show Premiered in 1994?",
+        choice: ["Frasier", "Friends", "Seinfeld", "Home Improvement"],
+        answer: 1,
+        photo: "assets/images/friends.gif"
+    },
+    {
+        question: "What's the Name of the Boy Band Whose First Single Was I Want You Back?",
+        choice: ["98 Degrees", "Backstreet Boys", "O-Town", "NSYNC"],
+        answer: 3,
+        photo: "assets/images/nsync.gif"
     },
     {
         question:
@@ -76,16 +80,10 @@ var questions = [
         answer:
         photo:
     },
-    {
-        question:
-        choice:
-        answer:
-        photo:
-    },
-]
+}
 
 //hide (How to hide parts of text) and reset function (will clear or reset the input form fields) to transition between question slides and start the game
-$("#reset").hide;
+$("#reset").hide();
 // Start the game by clicking on the "start" button
 $("#start").on("click", function () {
     $("#start").hide;
@@ -95,29 +93,27 @@ $("#start").on("click", function () {
             //The push() method adds new items to the end of an array, and returns the new length.
 	placeholder.push(questions[i]);
 }
-}
+})
 
 // Need to add function to start the timer
 function runTime() {
     if (!timerRunning) {
-        intervalId = setInterval(count, 3000);
+        intervalId = setInterval(decrement, 1000);
         clockRunning = true;
+    }
 }
-
 // Timer is started, now the countdown needs to begin (interval function)
 function decrement() {
     timeLeft --;
     $("#timer").html("<h2>Time Remaining: " + timeLeft + "</h2>");
-}
-
 // What happens if the timer reaches 0 before the person answers the question?
 if (timeLeft === 0) {
-    var questionNumber ++;
+    var questionNumber++;
     stop();
     $("#answer").html("<p>Time's up! The correct answer is: " + guess.choice[guess.answer] + "</p>");
 		hidepicture();
+    }
 }
-
 // Allow functionality to stop the timer
 function stop() {
     clearInterval(intervalId);
@@ -127,7 +123,24 @@ function stop() {
 // Is there where I put the questions array?
 function displayQuestion () {
     index = Math.floor(Math.random()*questions.length);
+    guess = questions[index];
+
+if () {
+    
+} else {
+    
 }
+
+
+
+}
+
+// Need to add a click event regarding answers
+$(".useranswer").on("click", function () {
+guess = 
+
+
+})
 
 // If/Else statement regarding whether user selects right or wrong answer
 if (guess === right) {
