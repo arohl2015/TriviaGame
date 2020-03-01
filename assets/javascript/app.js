7138a689d9adaae90e0ef54e00d46b8bbaa0973f
A Rohl <april.rohl2015@gmail.com>
2020-02-29 21:37:19 -0700

    added timer function in js

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,14 +1,14 @@
-// Beginning of game - start it
+//Start the game
 
 $(document).ready(function () {
-}
-)
+startGame()
+})
 
 // Creating the variables to track progress in game
 
 var questionNumber = 0;
-//will this read 15 seconds or do I need to enter in 1500?
-var timeLeft = 15;
+//will this read 20 seconds or do I need to enter in 2000?
+var timeLeft = 20;
 var questionCorrect = 0;
 var questionWrong = 0;
 
@@ -78,13 +78,28 @@ var questions = [
 ]
 
 // Start the game by clicking on the "start" button
+$("start").on("click", function () {
+// Loop through questions - is that done within the start function?
+}
 
-// Once you click on "start", then need to add function to start the timer
+// Need to add function to start the timer
+function runTime() {
+    if (!timerRunning) {
+        intervalId = setInterval(count, 2000);
+        clockRunning = true;
+}
 
 // Timer is started, now the countdown needs to begin (interval function)
+function decrement() {
+    timeLeft --;
+    $("#timer").html("<h2>Time Remaining: " + timeLeft + "</h2>");
+}
 
 // Allow functionality to stop the timer once it reaches 0
-
+function stop() {
+    clearInterval(intervalId);
+    timerRunning = false;
+  }
 // Within here will need to add in the vars relating to Q&A's
 // If/Else statement to clarify if user is correct or wrong 
 if (condition) {
